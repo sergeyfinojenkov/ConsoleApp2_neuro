6446ba12f83070309db8315facce141ed330776e
sergeyfinojenkov <sergeyfinojenkov@yandex.ru>
2021-11-13 01:43:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2_neuro/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp2_neuro
+{
+    class Program
+    {
+
+        static double Act(double input)
+        {
+            return 1 / (1 + Math.Pow(Math.E, (-1) * input));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First N");
+
+            var w1 = new double[,]
+            {
+                { 0.2, 0.2, 0.2 },
+                { 0.4, 0.4, 0.4 },
+                { 0.6, 0.6, 0.6}
+            };
+
+            var w2 = new double[] { 0.5, 0.5, 0.5 };
+            
+            var b1 = new double[] { 0.8, 0.8, 0.8 };
+
+            var b2 = new double[] { 0.2 };
+
+
+
+
+
+        }
+    }
+}
